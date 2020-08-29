0e554799842e63472ac3bc309e88eb542e2edab4
Anna Romanovska <sup2a1nn@gmail.com>
2020-08-29 22:04:57 +0300

    Start to build docker image1

--- app/app/settings.py
+++ app/app/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-# from .mail_auth import EMAIL_HOST_USER, EMAIL_HOST_PASSWORD
 from django.contrib.messages import constants as messages
 import os
 
